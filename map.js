37c118d4ba422ab67c73803e8d68e73ee677958f
Yuqing <Yuqing@v1271-0a9c4fb6.wifi.cf.ac.uk>
2016-06-11 14:32:31 +0100

    updated map template

--- map.js
+++ map.js
@@ -1,12 +1,14 @@
 var map = L.map('map',{
-    center: [1.505, -0.09],
-    zoom: 1
+    center: [51.4752006531,-3.1733899117],
+    zoom: 13
 });
 
-
-L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
-attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
-}).addTo(map);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	    attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	    maxZoom: 18,
+	    id: 'trekwho.o0pmdp8k',
+	    accessToken: 'pk.eyJ1IjoidHJla3dobyIsImEiOiJjaWdhcTVyZW8wM2N1d2RtN2E5ZHB1bWdkIn0.Hp0qtRYfZegcf4rn4FL70Q'
+	}).addTo(map);
 
 // map.on('popupopen', function(centreMarker) {
 //     var cM = map.project(centreMarker.popup._latlng);
