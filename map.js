8141b45d67c826c98e3ceda0ffd66542ad8347d7
imandimyati <ImanDimyati@Nuruls-MacBook-Pro.local>
2016-06-12 01:47:56 +0100

    fixed script source problem
    Because keep getting: Uncaught TypeError: L.markerClusterGroup is not a
    function and Uncaught ReferenceError: L is not defined

--- map.js
+++ map.js
@@ -1,6 +1,7 @@
+
 var map = L.map('map',{
     center: [51.4892006531,-3.1733899117],
-    zoom: 13
+    zoom: 14
 });
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -31,23 +32,20 @@ function drawMarker(data){
   });
 
 var markerClusters = L.markerClusterGroup();
- 
+
 for ( var i = 0; i < data.length; ++i )
 {
   if(data[i]['location'] != ''){
   var loc = JSON.parse(data[i]['location']);
   var content = '<p class="text">' + data[i]['tweet_text'] + '</p><img class="pic" src="' + data[i]['picture_url']+ '">';
- 
+
   var m = L.marker([loc[0], loc[1]])
                   .bindPopup(content);
- 
+
   markerClusters.addLayer(m);
   }
 }
- 
+
 map.addLayer( markerClusters );
 
 }
-
-
-
